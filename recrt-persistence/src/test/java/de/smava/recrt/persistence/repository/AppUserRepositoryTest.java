bf212197940734cc36d28d110bb82eed19a73dba
Rafael Rodriguez <rafael.rodriguez@flightright.de>
2016-05-17 13:15:03 +0200

    task-4 implemented persistence tests.

--- recrt-persistence/src/test/java/de/smava/recrt/persistence/repository/AppUserRepositoryTest.java
+++ recrt-persistence/src/test/java/de/smava/recrt/persistence/repository/AppUserRepositoryTest.java
@@ -1,22 +1,50 @@
 package de.smava.recrt.persistence.repository;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+import de.smava.recrt.persistence.config.PersistenceConfig;
+import de.smava.recrt.persistence.model.AppUserEntity;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = PersistenceConfig.class)
 public class AppUserRepositoryTest {
 
+	@Autowired
+	AppUserRepository appUserRepository;
+	
     @Test
     public void testSave() throws Exception {
-
+    	AppUserEntity user = new AppUserEntity("testuser");
+    	user.setEmail("testuser@test.com");
+    	user.setPassword("1234");
+    	appUserRepository.save(user);
+    	AppUserEntity result = appUserRepository.findOne("testuser");
+        assertEquals(result.getUsername(), "testuser");
     }
 
     @Test
     public void testFindOne() throws Exception {
-
+    	AppUserEntity result = appUserRepository.findOne("user1");
+        assertEquals(result.getEmail(), "user1@smava.de");
     }
 
     @Test
     public void testDelete() throws Exception {
-
+    	AppUserEntity user = new AppUserEntity("testuser");
+    	user.setEmail("testuser@test.com");
+    	user.setPassword("1234");
+    	appUserRepository.save(user);
+    	AppUserEntity result = appUserRepository.findOne("testuser");
+        assertEquals(result.getUsername(), "testuser");
+    	appUserRepository.delete("testuser");
+    	assertNull(appUserRepository.findOne("testuser"));
     }
 
 }
--- recrt-persistence/src/test/java/de/smava/recrt/persistence/repository/AppUserRoleRepositoryTest.java
+++ recrt-persistence/src/test/java/de/smava/recrt/persistence/repository/AppUserRoleRepositoryTest.java
@@ -1,11 +1,31 @@
 package de.smava.recrt.persistence.repository;
 
-import org.junit.Test;
+import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import de.smava.recrt.persistence.config.PersistenceConfig;
+import de.smava.recrt.persistence.model.AppUserRoleEntity;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = PersistenceConfig.class)
 public class AppUserRoleRepositoryTest {
 
+	@Autowired
+	AppUserRoleRepository appUserRoleRepository;
+	
+	@Autowired
+	AppUserRepository appUserRepository;
+	
     @Test
     public void testFindByKeyAppUser() throws Exception {
-
+    	List<AppUserRoleEntity> result = appUserRoleRepository.findByKeyAppUser(appUserRepository.findOne("user1"));
+    	assertEquals(1, result.size());
     }
 }
