457dec51862ba60dabf2da22c99463273b7b7c13
Rafael Rodriguez <rafael.rodriguez@flightright.de>
2016-05-17 13:00:53 +0200

    task-2 Add a name for "users" cache and method getByAppUser is now cacheable. TODO evict method, add a key for the cache method, etc.

--- recrt-service/src/main/java/de/smava/recrt/service/config/CachingConfig.java
+++ recrt-service/src/main/java/de/smava/recrt/service/config/CachingConfig.java
@@ -14,6 +14,6 @@ public class CachingConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager();
+        return new ConcurrentMapCacheManager("users");
     }
 }
